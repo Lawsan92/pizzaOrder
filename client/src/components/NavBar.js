c67f9c75f4c5b9fc1a6ecee956d24b390e895ed3
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-10-22 12:56:42 -0500

    testing out multipage navigation

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div id='navbar'>
-      'NavBar'
+    <div className='nav'>
+      <a href='/' className='site-title'>
+        Site Title
+      </a>
+      <ul>
+        <li className='active'>
+          <a href='/pricing'>Pricing</a>
+        </li>
+        <li>
+        <a href='/about'>About</a>
+        </li>
+      </ul>
     </div>
   )
 }
