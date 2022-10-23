ae9c4f5e3d71f0d46ced3fa8811de6c754422335
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-10-23 18:43:49 -0500

    orders endpoint now properly parses json data from request

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const axios = require('axios');
 require('dotenv').config();
 
+const app = express();
+
+// ---Middleware---
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+// ---Route Handlers---
 app.get('/test', (req, res) => {
-  res.status(200).send('hello world');
+  res.sendStatus(200).send('hello world');
 });
 
 app.get('/backorders', (req, res) => {
@@ -20,13 +26,16 @@ app.get('/backorders', (req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
+app.post('/orders', (req, res) => {
+  res.status(200).send('order up!');
+  console.log('req.data:', req.body);
+});
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`listening to port: ${process.env.PORT}`)
 });
-
-app.use(express.json());
 
 app.get('/*', function(req, res) { // <-- add
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
@@ -35,3 +44,10 @@ app.get('/*', function(req, res) { // <-- add
     }
   })
 });
+
+/*
+curl -d '{"Crust": "NORMAL", "Flavor": "CHEESE", "Order_ID": 1, "Size": "M", "Table_No": 1, "Timestamp": "2022-09-03T18:21:08.710006"}' -H "Content-Type: application/json" -POST "localhost:3000/orders"
+
+curl -d '{}' -H "Content-Type: application/json" -POST "localhost:3000/orders"
+}
+*/
