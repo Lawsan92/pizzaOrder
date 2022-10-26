43fc4e52bb0bdceaa1936a74a8f96e31137dc962
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-10-26 18:53:24 -0500

    cleaning up

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -8,14 +8,73 @@ import History from './pages/History.js';
 import Home from './pages/Home.js';
 import Cart from './pages/Cart.js';
 
-const Router = ({ login, getUserCred, user, order, updateOrder, orderHist, getOrderHist, cancelOrder, cancel, isReady, getHistory, cart, addToCart, checkoutCart, token }) => {
+const Router = ({
+  login,
+  getUserCred,
+  user,
+  order,
+  updateOrder,
+  orderHist,
+  getOrderHist,
+  cancelOrder,
+  cancel,
+  isReady,
+  getHistory,
+  cart,
+  addToCart,
+  checkoutCart,
+  token }) => {
   return (
   <Routes>
-    <Route path='/' element={<Home user={user} token={token}/>} />
-    <Route path='login' element={<Login login={login} getUserCred={ getUserCred } user={user} token={token}/>} />
-    <Route path='/history' element={<History orderHist={orderHist} getOrderHist={getOrderHist} order={order} updateOrder={updateOrder} cancelOrder={cancelOrder} cancel={cancel} getHistory={getHistory} user={user} token={token}/>} />
-    <Route path='/checkout' element={<Checkout order={order} updateOrder={updateOrder} isReady={isReady} addToCart={addToCart} cart={cart} user={user} token={token}/>} />
-    <Route path='/cart' element={<Cart cart={cart} isReady={isReady} updateOrder={updateOrder} order={order} checkoutCart={checkoutCart} user={user} token={token}/>} />
+    <Route
+    path='/'
+    element={
+      <Home
+      user={user}
+      token={token}
+      />} />
+    <Route
+    path='login'
+    element={
+      <Login
+      login={login}
+      getUserCred={ getUserCred }
+      user={user}
+      token={token}/>} />
+    <Route
+    path='/history'
+    element={
+      <History
+      orderHist={orderHist}
+      getOrderHist={getOrderHist}
+      order={order}
+      updateOrder={updateOrder}
+      cancelOrder={cancelOrder}
+      cancel={cancel}
+      getHistory={getHistory}
+      user={user}
+      token={token}/>} />
+    <Route
+    path='/checkout'
+    element={
+      <Checkout
+      order={order}
+      updateOrder={updateOrder}
+      isReady={isReady}
+      addToCart={addToCart}
+      cart={cart}
+      user={user}
+      token={token}/>} />
+    <Route
+    path='/cart'
+    element={
+      <Cart
+      cart={cart}
+      isReady={isReady}
+      updateOrder={updateOrder}
+      order={order}
+      checkoutCart={checkoutCart}
+      user={user} token={token}/>} />
   </Routes>
   );
 }
