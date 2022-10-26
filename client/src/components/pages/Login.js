4e7ab26f2693df746c31e3e229542253f4c8c47d
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-10-26 15:41:21 -0500

    added increased password authentication protocols

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -3,6 +3,40 @@ import NavBar from './NavBar';
 
 const Login = ({ login, getUserCred, user, token }) => {
 
+
+  const validPassword = () => {
+    const password = user.password;
+    const [lowerAlpha, upperAlpha, nums, chars ] =
+    ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', "01234567890", '!@#$%^&*()'];
+    let [check1, check2, check3, check4] = [0, 0, 0, 0];
+    for (let char of password) {
+      if (lowerAlpha.indexOf(char) > - 1) {
+        check1 ++;
+      }
+      if (upperAlpha.indexOf(char) > - 1) {
+        check2 ++;
+      }
+      if (nums.indexOf(char) > - 1) {
+        check3 ++;
+      }
+      if (chars.indexOf(char) > - 1) {
+        check4 ++;
+      }
+    }
+    if (check1 && check2 && check3 && check4) {
+      login (true)
+    } else {
+      alert ('Invalid Password...check console');
+      console.log(`
+       needs:
+         a lower-cased letter
+         an upper-cased letter
+         an integer
+         a special character: ${chars}
+      `);
+    }
+  }
+
   return (
     <div id='login'>
       <NavBar user={user} token={token}/>
@@ -13,7 +47,7 @@ const Login = ({ login, getUserCred, user, token }) => {
         <h3>password</h3>
         <input type='text' onChange={(e) => {getUserCred({...user, password: e.target.value})}}></input>
       </form>
-      <button className='login-button' onClick={() => {login (true)}}>Login</button>
+      <button className='login-button' onClick={() => {validPassword ()}}>Login</button>
     </div>
   );
 }
